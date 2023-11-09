f9cc0bcf8b5836da290fcec79e55f4edc8caa4c9
hedys9 <hedysadeghi@gmail.com>
2023-11-09 15:55:33 +0100

    bonus casual number

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 let name = prompt("What's your name?");
 let surname = prompt("What's your surname?");
 let favoriteColor= prompt("What's your favorite color?")
@@ -7,6 +8,27 @@ let userNumber2 = prompt ("Choose a second number: ");
 let fourthPasswordElement= parseInt(userNumber1)/parseInt(userNumber2);
 
 let passwordCreated= "{" +name + "}" + "{" + surname + "}" + "{" +favoriteColor+ "}" +fourthPasswordElement;
+
+console.log(passwordCreated);
+
+document.getElementById("userPasswordCreated").innerHTML= passwordCreated;
+*/
+
+
+let name = prompt("What's your name?");
+let surname = prompt("What's your surname?");
+let favoriteColor= prompt("What's your favorite color?")
+
+
+// numero casuale tra 0 e 1 
+let randomNumber = Math.random();
+
+//numero casuale tra 0 e 1000
+randomNumber= Math.floor(Math.random()*1000)
+
+console.log(randomNumber); 
+
+let passwordCreated= "{" +name + "}" + "{" + surname + "}" + "{" +favoriteColor+ "}"+ randomNumber ;
 
 console.log(passwordCreated);
 
